fec4c4c8094f5d544409c933f49a416def25a8ea
Tobias Burger <tobias.burger@code.berlin>
2026-03-25 17:41:43 +0100

    changed and addedd a bunch of stuff that fried my brain

--- server.js
+++ server.js
@@ -3,33 +3,49 @@ const {first} = require('./utils/pgn-parser');
 const db = require('./db/init');
 const app = express();
 const port = 3000; 
+
+
 app.use(express.urlencoded({ extended: true }));
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
 
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html');
+    res.render('index');
 });
 
 app.get('/games', (req, res) => {
-    res.sendFile(__dirname + '/public/games.html');
-    console.log(db.prepare("SELECT * FROM Game").all());
+    const games = db.prepare("SELECT * FROM Game").all();
+    res.render('games', { games: games });
 });
 
 app.get('/games/:id', (req, res) => {
-    const id= req.params.id;
-        res.send ("Game detail for game " + id);
-})
+    const id = req.params.id;
+    const game = db.prepare("SELECT * FROM Game WHERE id = ?").get(id);
+    const moves = db.prepare("SELECT * FROM Move WHERE game_id = ?").all(id);
+    res.render('game-detail', { game: game, moves: moves });
+});
 
 app.post('/games', (req, res) => {
+    if (!req.body.pgn) {
+        return res.redirect('/import');
+    }
    const result = first(req.body.pgn);
-   db.prepare (`INSERT INTO game (pgn_raw, white_player, black_player, result, date_played, white_elo, black_elo, site, opening) 
+   const moves = result.history;
+   const game_insert = db.prepare(`INSERT INTO game (pgn_raw, white_player, black_player, result, date_played, white_elo, black_elo, site, opening) 
         VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)`).run(req.body.pgn, result.header.White, result.header.Black, result.header.Result, result.header.Date, result.header.WhiteElo, result.header.BlackElo, result.header.Site, result.header.Opening);
-   res.redirect('/games');
-})
+    const gameId = game_insert.lastInsertRowid;
+    const move_insert = db.prepare(`INSERT INTO Move (game_id, move_number, color, san, fen_after) VALUES (?, ?, ?, ?, ?)`);
+    moves.forEach((move, index) => {
+        move_insert.run(gameId, Math.floor(index / 2)+1, index % 2 === 0 ? 'w' : 'b', move, null);
+    });
+    res.redirect('/games');
+});
 
 app.get('/import', (req, res) => {
-    res.sendFile(__dirname + '/public/import.html');
+    res.render('import');
 });
 
 app.listen(port, () => {
