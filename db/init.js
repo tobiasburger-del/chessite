2e3086480d138ca4ea58150e1e7b85b513da5f8b
Tobias Burger <tobias.burger@code.berlin>
2026-04-09 18:58:57 +0200

    changed db tables for login security functionality

--- db/init.js
+++ db/init.js
@@ -4,8 +4,14 @@ const { Pool } = require('pg')
 const pool = new Pool ({connectionString: process.env.DATABASE_URL})
 
 async function initDB() {
+    await pool.query(`CREATE TABLE IF NOT EXISTS users(
+        id SERIAL PRIMARY KEY,
+        email TEXT UNIQUE NOT NULL,
+        password_hash TEXT NOT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP)`)
     await pool.query(`CREATE TABLE IF NOT EXISTS game(
         id SERIAL PRIMARY KEY, 
+        user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
         pgn_raw TEXT,
         white_player TEXT,
         black_player TEXT,
@@ -21,7 +27,7 @@ async function initDB() {
         
     await pool.query(`CREATE TABLE IF NOT EXISTS move(
         id SERIAL PRIMARY KEY,
-        game_id INTEGER REFERENCES Game(id),
+        game_id INTEGER REFERENCES Game(id) ON DELETE CASCADE,
         move_number INTEGER,
         color TEXT,
         san TEXT,
