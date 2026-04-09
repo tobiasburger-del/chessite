0c6b450ea8adb06441449617024349aec47d7f41
Tobias Burger <tobias.burger@code.berlin>
2026-04-09 15:05:05 +0200

    addedd partials for my nav bar

--- server.js
+++ server.js
@@ -1,71 +1,120 @@
-const express = require('express');
-const {first} = require('./utils/pgn-parser');
-const pool = require('./db/init');
+const express = require("express");
+const { first } = require("./utils/pgn-parser");
+const pool = require("./db/init");
 const app = express();
-const port = 3000; 
-
+const port = 3000;
 
 app.use(express.urlencoded({ extended: true }));
 
-app.set('view engine', 'ejs');
-app.set('views', './views');
+app.set("view engine", "ejs");
+app.set("views", "./views");
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-    res.render('index');
+app.get("/", (req, res) => {
+  res.render("index");
 });
 
-app.get('/games', async (req, res) => {
+app.get("/games", async (req, res) => {
+  try {
     const games = await pool.query("SELECT * FROM Game");
-    res.render('games', { games: games.rows });
+    res.render("games", { games: games.rows });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("That went wrong!");
+  }
 });
 
-app.get('/games/:id', async (req, res) => {
+app.get("/games/:id", async (req, res) => {
+  try {
     const id = req.params.id;
     const game = await pool.query("SELECT * FROM Game WHERE id = $1", [id]);
-    const moves = await pool.query("SELECT * FROM Move WHERE game_id = $1", [id]);
-    res.render('game-detail', { game: game.rows[0], moves: moves.rows });
+    const moves = await pool.query("SELECT * FROM Move WHERE game_id = $1", [
+      id,
+    ]);
+    res.render("game-detail", { game: game.rows[0], moves: moves.rows });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Something went wrong");
+  }
 });
 
-app.post('/games', async (req, res) => {
+app.post("/games", async (req, res) => {
+  try {
     if (!req.body.pgn) {
-        return res.redirect('/import');
+      return res.redirect("/import");
     }
-   const result = first(req.body.pgn);
-   const exists = await pool.query("SELECT id FROM Game WHERE pgn_raw = $1", [req.body.pgn]);
-   if (exists.rows[0]) {
-       return res.redirect('/import');
-   }
-   const moves = result.history;
-   const game_insert = await pool.query(`INSERT INTO game (pgn_raw, white_player, black_player, result, date_played, white_elo, black_elo, site, opening) 
-        VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING id`, [req.body.pgn, result.header.White, result.header.Black, result.header.Result, result.header.Date, result.header.WhiteElo, result.header.BlackElo, result.header.Site, result.header.Opening]);
+    const result = first(req.body.pgn);
+    const exists = await pool.query("SELECT id FROM Game WHERE pgn_raw = $1", [
+      req.body.pgn,
+    ]);
+    if (exists.rows[0]) {
+      return res.redirect("/import");
+    }
+    const moves = result.history;
+    const game_insert = await pool.query(
+      `INSERT INTO game (pgn_raw, white_player, black_player, result, date_played, white_elo, black_elo, site, opening) 
+        VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING id`,
+      [
+        req.body.pgn,
+        result.header.White,
+        result.header.Black,
+        result.header.Result,
+        result.header.Date,
+        result.header.WhiteElo,
+        result.header.BlackElo,
+        result.header.Site,
+        result.header.Opening,
+      ],
+    );
     const gameId = game_insert.rows[0].id;
     for (const [index, move] of moves.entries()) {
-        await pool.query(`INSERT INTO move (game_id, move_number, color, san, fen_after) VALUES ($1, $2, $3, $4, $5)`, [gameId, Math.floor(index / 2) + 1,index % 2 === 0 ? 'w' : 'b', move, null]);
+      await pool.query(
+        `INSERT INTO move (game_id, move_number, color, san, fen_after) VALUES ($1, $2, $3, $4, $5)`,
+        [
+          gameId,
+          Math.floor(index / 2) + 1,
+          index % 2 === 0 ? "w" : "b",
+          move,
+          null,
+        ],
+      );
     }
-    res.redirect('/games');
+    res.redirect("/games");
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Something went wrong!");
+  }
 });
 
-app.post('/games/:id/delete', async (req, res) => {
+app.post("/games/:id/delete", async (req, res) => {
+  try {
     const id = req.params.id;
     const move = await pool.query("DELETE FROM Move WHERE game_id = $1", [id]);
     const game = await pool.query("DELETE FROM Game WHERE id = $1", [id]);
-    res.redirect('/games')
-
+    res.redirect("/games");
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Something went wrong!");
+  }
 });
 
-app.post('/games/:id/edit', async (req, res) => {
+app.post("/games/:id/edit", async (req, res) => {
+  try {
     const id = req.params.id;
-    const notes = req.body.notes
+    const notes = req.body.notes;
     await pool.query("UPDATE Game SET notes = $1 WHERE id = $2", [notes, id]);
-    res.redirect('/games/'+id)
-})
+    res.redirect("/games/" + id);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Something went wrong!");
+  }
+});
 
-app.get('/import', (req, res) => {
-    res.render('import');
+app.get("/import", (req, res) => {
+  res.render("import");
 });
 
 app.listen(port, () => {
-    console.log(`Server is running and listening on port ${port}`);
+  console.log(`Server is running and listening on port ${port}`);
 });
